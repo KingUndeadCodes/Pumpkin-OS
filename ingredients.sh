b191388cd98f7033a65a1284956a4ff57184835b
KingUndeadCodes <70785950+KingUndeadCodes@users.noreply.github.com>
2021-02-16 13:52:14 -0600

    Create ingredients.sh

---
+++ ingredients.sh
@@ -0,0 +1,8 @@
+# Debian / Ubuntu
+
+sudo apt update
+sudo apt-get install git
+sudo apt-get install bash
+sudo apt-get install nasm
+sudo apt-get install qemu-system
+sudo apt-get install build-essential
