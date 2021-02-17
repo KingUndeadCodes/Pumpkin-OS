e98ad5a8681f1ecba6728ed68ea45ab49f77f685
KingUndeadCodes <70785950+KingUndeadCodes@users.noreply.github.com>
2021-02-17 09:41:55 -0600

    Update ingredients.sh

--- ingredients.sh
+++ ingredients.sh
@@ -11,6 +11,13 @@ sudo apt-get install libmpfr-dev
 sudo apt-get install qemu-system
 sudo apt-get install build-essential
 
+# Arch Linux (Still working on it)
+
+sudo pacman -Syu
+sudo pacman -S git
+sudo pacman -S flex
+sudo pacman -S bash
+sudo pacman -S nasm
+
 echo -e "\033[0;32mPackage installation has finished! | Sleeping for 5 seconds.\033[0m"
 sleep 5
-
