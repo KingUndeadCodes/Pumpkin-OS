32fab4fc565c3809e9d170abc503fb3eda6c3d89
KingUndeadCodes <70785950+KingUndeadCodes@users.noreply.github.com>
2021-02-17 16:07:53 -0500

    Add files via upload

--- 32-bit/p-kernel.cpp
+++ 32-bit/p-kernel.cpp
@@ -1,11 +1,33 @@
-// Copyright KingUndeadCodes, 2021
-
 extern "C" void main(){
-   *((char*)0xb8000) = 'P'; // First      Char  : "H"
-   *((char*)0xb8002) = 'O'; // Second     Char  : "E"
-   *((char*)0xb8004) = 'G'; // Third      Char  : "L"
-   *((char*)0xb8006) = '!'; // Fourth     Char  : "L"
-   *((char*)0xb8008) = '!'; // Fifth      Char  : "O"
+   *((char*)0xb8000) = 'H';
+   *((char*)0xb8002) = 'E';
+   *((char*)0xb8004) = 'L';
+   *((char*)0xb8006) = 'L';
+   *((char*)0xb8008) = 'O';
+   *((char*)0xb800a) = ' ';
+   *((char*)0xb800c) = 'W';
+   *((char*)0xb800e) = 'O';
+   *((char*)0xb8010) = 'R';
+   *((char*)0xb8012) = 'L';
+   *((char*)0xb8014) = 'D';
+   *((char*)0xb8016) = '!';
+   *((char*)0xb8018) = ' ';
+   *((char*)0xb801a) = 'T';
+   *((char*)0xb801c) = 'H';
+   *((char*)0xb801e) = 'I';
+   *((char*)0xb8020) = 'S';
+   *((char*)0xb8022) = ' ';
+   *((char*)0xb8024) = 'I';
+   *((char*)0xb8026) = 'S';
+   *((char*)0xb8028) = ' ';
+   *((char*)0xb802a) = 'A';
+   *((char*)0xb802c) = ' ';
+   *((char*)0xb802e) = 'T';
+   *((char*)0xb8030) = 'E';
+   *((char*)0xb8032) = 'S';
+   *((char*)0xb8034) = 'T';
+   *((char*)0xb8036) = '!';
    return;
 }
+
 
