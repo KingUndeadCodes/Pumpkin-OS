c8e2ca0c0f073cb79a3b9898363cc74ea1a6ef48
KingUndeadCodes <70785950+KingUndeadCodes@users.noreply.github.com>
2021-02-16 20:41:00 -0600

    Update ingredients.sh

--- ingredients.sh
+++ ingredients.sh
@@ -2,8 +2,12 @@
 
 sudo apt update
 sudo apt-get install git
+sudo apt-get install flex
 sudo apt-get install bash
 sudo apt-get install nasm
+sudo apt-get install libmpc-dev
+sudo apt-get install libgmp3-dev
+sudo apt-get install libmpfr-dev
 sudo apt-get install qemu-system
 sudo apt-get install build-essential
 
