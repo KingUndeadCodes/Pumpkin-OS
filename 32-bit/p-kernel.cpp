42d83f6815a297bb433a4c6ae06fb2d651453804
KingUndeadCodes <70785950+KingUndeadCodes@users.noreply.github.com>
2021-02-18 20:28:21 -0500

    Add files via upload

--- 32-bit/p-kernel.cpp
+++ 32-bit/p-kernel.cpp
@@ -1,29 +1,72 @@
+/*
+
+   Copyright (c) 2020 KingUndeadCodes
+   Protected under MIT License which lays down the terms of use.
+
+*/
+
 extern "C" void main(){
 
-   *((int*)0xb8001) = 0x0f;
-   *((int*)0xb8003) = 0x0f;
-   *((int*)0xb8005) = 0x0f;
-   *((int*)0xb8007) = 0x0f;
-   *((int*)0xb8009) = 0x0f;
-   *((int*)0xb800b) = 0x0f;
-   *((int*)0xb800d) = 0x0f;
-   *((int*)0xb800f) = 0x0f;
-   *((int*)0xb8011) = 0x0f;
-   *((int*)0xb8013) = 0x0f;
-   *((int*)0xb8015) = 0x0f;
+   const char *name = "Flying Chicken || Revision 1.0.0";
+   const bool found = 1;
 
-   *((char*)0xb8000) = 'F';
-   *((char*)0xb8002) = 'o';
-   *((char*)0xb8004) = 'u';
-   *((char*)0xb8006) = 'n';
-   *((char*)0xb8008) = 'd';
-   *((char*)0xb800a) = ':';
-   *((char*)0xb800c) = ' ';
-   *((char*)0xb800e) = 'T';
-   *((char*)0xb8010) = 'r';
-   *((char*)0xb8012) = 'u';
-   *((char*)0xb8014) = 'e';
+   if (found == true) {
 
-   return;
+      *((int*)0xb8001) = 0x0f;
+      *((int*)0xb8003) = 0x0f;
+      *((int*)0xb8005) = 0x0f;
+      *((int*)0xb8007) = 0x0f;
+      *((int*)0xb8009) = 0x0f;
+      *((int*)0xb800b) = 0x0f;
+      *((int*)0xb800d) = 0x0f;
+      *((int*)0xb800f) = 0x02f;
+      *((int*)0xb8011) = 0x02f;
+      *((int*)0xb8013) = 0x22f;
+      *((int*)0xb8015) = 0x02f;
 
+      *((char*)0xb8000) = 'F';
+      *((char*)0xb8002) = 'o';
+      *((char*)0xb8004) = 'u';
+      *((char*)0xb8006) = 'n';
+      *((char*)0xb8008) = 'd';
+      *((char*)0xb800a) = ':';
+      *((char*)0xb800c) = ' ';
+      *((char*)0xb800e) = 'T';
+      *((char*)0xb8010) = 'r';
+      *((char*)0xb8012) = 'u';
+      *((char*)0xb8014) = 'e';
+
+      return;
+
+   } else {
+
+      *((int*)0xb8001) = 0x0f;
+      *((int*)0xb8003) = 0x0f;
+      *((int*)0xb8005) = 0x0f;
+      *((int*)0xb8007) = 0x0f;
+      *((int*)0xb8009) = 0x0f;
+      *((int*)0xb800b) = 0x0f;
+      *((int*)0xb800d) = 0x0f;
+      *((int*)0xb800f) = 0x04f;
+      *((int*)0xb8011) = 0x04f;
+      *((int*)0xb8013) = 0x04f;
+      *((int*)0xb8015) = 0x04f;
+      *((int*)0xb8017) = 0x04f;
+
+      *((char*)0xb8000) = 'F';
+      *((char*)0xb8002) = 'o';
+      *((char*)0xb8004) = 'u';
+      *((char*)0xb8006) = 'n';
+      *((char*)0xb8008) = 'd';
+      *((char*)0xb800a) = ':';
+      *((char*)0xb800c) = ' ';
+      *((char*)0xb800e) = 'F';
+      *((char*)0xb8010) = 'a';
+      *((char*)0xb8012) = 'l';
+      *((char*)0xb8014) = 's';
+      *((char*)0xb8016) = 'e';
+
+      return;
+
+   }
 }
