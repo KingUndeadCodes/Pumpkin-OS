583f51121a6d439e510a6996ddc9b64ca0d110a3
KingUndeadCodes <70785950+KingUndeadCodes@users.noreply.github.com>
2021-02-22 12:48:11 -0500

    Update ingredients.sh

--- ingredients.sh
+++ ingredients.sh
@@ -1,9 +1,7 @@
 # Debian / Ubuntu (ONLY)
 
 sudo apt update
-sudo apt-get install git
 sudo apt-get install flex
-sudo apt-get install bash
 sudo apt-get install nasm
 sudo apt-get install libmpc-dev
 sudo apt-get install libgmp3-dev
@@ -11,18 +9,4 @@ sudo apt-get install libmpfr-dev
 sudo apt-get install qemu-system
 sudo apt-get install build-essential
 
-# Arch Linux (Still working on it)
-
-sudo pacman -Syu
-sudo pacman -S gcc
-sudo pacman -S gmp
-sudo pacman -S git
-sudo pacman -S qemu
-sudo pacman -S flex
-sudo pacman -S mpfr
-sudo pacman -S bash
-sudo pacman -S nasm
-sudo pacman -S libmpc
-
-echo -e "\033[0;32mPackage installation has finished! | Sleeping for 5 seconds.\033[0m"
-sleep 5
+echo -e "\033[0;32mPackage installation has finished!\033[0m\n"
