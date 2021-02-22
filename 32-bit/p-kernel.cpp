dbf449b82353a181ddfabdbb0122067c9bc912ef
KingUndeadCodes <70785950+KingUndeadCodes@users.noreply.github.com>
2021-02-22 13:10:56 -0500

    Update p-kernel.cpp

--- 32-bit/p-kernel.cpp
+++ 32-bit/p-kernel.cpp
@@ -1,23 +1,3 @@
-#include <stdint.h>
- 
-enum video_type
-{
-    VIDEO_TYPE_NONE = 0x00,
-    VIDEO_TYPE_COLOUR = 0x20,
-    VIDEO_TYPE_MONOCHROME = 0x30,
-};
- 
-uint16_t detect_bios_area_hardware(void)
-{
-    const uint16_t* bda_detected_hardware_ptr = (const uint16_t*) 0x410;
-    return *bda_detected_hardware_ptr;
-}
- 
-enum video_type get_bios_area_video_type(void)
-{
-    return (enum video_type) (detect_bios_area_hardware() & 0x30);
-}
-
 /// Copyright (c) 2020 KingUndeadCodes
 /// Protected under MIT License which lays down the terms of use.
 
