80f743ab2ba5378831d0037864a01370eab288ce
KingUndeadCodes <70785950+KingUndeadCodes@users.noreply.github.com>
2021-03-01 13:38:54 -0600

    Update ingredients.sh

--- etc/ingredients.sh
+++ etc/ingredients.sh
@@ -1,12 +1,44 @@
-# Debian / Ubuntu (ONLY)
+# FROM: mello-pax/MellOs
+# GCC cross compiler for i386 systems (might take quite some time, prepare food)
 
 sudo apt update
-sudo apt-get install flex
-sudo apt-get install nasm
-sudo apt-get install libmpc-dev
-sudo apt-get install libgmp3-dev
-sudo apt-get install libmpfr-dev
-sudo apt-get install qemu-system
-sudo apt-get install build-essential
+sudo apt install build-essential
+sudo apt install bison
+sudo apt install flex
+sudo apt install libgmp3-dev
+sudo apt install libmpc-dev
+sudo apt install libmpfr-dev
+sudo apt install texinfo
+
+export PREFIX="/usr/local/i386elfgcc"
+export TARGET=i386-elf
+export PATH="$PREFIX/bin:$PATH"
+
+mkdir /tmp/src
+cd /tmp/src
+curl -O http://ftp.gnu.org/gnu/binutils/binutils-2.35.1.tar.gz
+tar xf binutils-2.35.1.tar.gz
+mkdir binutils-build
+cd binutils-build
+../binutils-2.35.1/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX 2>&1 | tee configure.log
+sudo make all install 2>&1 | tee make.log
+
+cd /tmp/src
+curl -O https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz
+tar xf gcc-10.2.0.tar.gz
+mkdir gcc-build
+cd gcc-build
+echo Configure: . . . . . . .
+../gcc-10.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-language=c++ --without-headers
+echo MAKE ALL-GCC:
+sudo make all-gcc
+echo MAKE ALL-TARGET-LIBGCC:
+sudo make all-target-libgcc
+echo MAKE INSTALL-GCC:
+sudo make install-gcc
+echo MAKE INSTALL-TARGET-LIBGCC:
+sudo make install-target-libgcc
+echo HERE U GO MAYBE:
+ls /usr/local/i386elfgcc/bin
 
 echo -e "\033[0;32mPackage installation has finished!\033[0m\n"
