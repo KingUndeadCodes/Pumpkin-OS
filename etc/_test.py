aee487f96696a9bacccc9f17e588bce38d9e983b
KingUndeadCodes <70785950+KingUndeadCodes@users.noreply.github.com>
2021-03-02 09:01:02 -0600

    Update _test.py

--- etc/_test.py
+++ etc/_test.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python
-
+# #!/usr/bin/env python
 import platform
 import os
 
 def main():
   if platform.system == 'Linux':
-    os.system('bash ingredients.sh')
-  
-  
+    if platform.platform.linux_distribution[0] == 'Ubuntu':
+      os.system('bash ingredients.sh')
+    
 if __name__ == "__main__":
   main()
