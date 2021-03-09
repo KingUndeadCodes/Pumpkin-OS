b4e762c6b6a6c07f9cce040003eca0e5bb8a3f23
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-03-09 17:26:59 -0600

    Add files via upload

--- 32-bit/p-kernel.cpp
+++ 32-bit/p-kernel.cpp
@@ -1,26 +1,23 @@
 /// Copyright (C) 2021 KingUndeadCodes (https://github.com/KingUndeadCodes)
 /// Protected under MIT License which lays down the terms of use.
 
-#include <stdint.h>
-
 void system_print(char* str) {
-  char* video_memory = (char*) 0xb8000;
-  char string[] = "Welcome to Pumpkin OS! (Flying Chicken || Revision 1.0.2)";
-  int length = sizeof(string) / sizeof(char);
-  for (int i = 0; i < length; i++) {
-     *video_memory = string[i];
-     video_memory += 2;
-  }
+   char* video_memory = (char*) 0xb8000;
+   char string[] = "Welcome to Pumpkin OS! (Flying Chicken || Revision 1.0.2)";
+   int length = sizeof(string) / sizeof(char);
+   for (int i = 0; i < length; i++) {
+      *video_memory = string[i];
+      video_memory += 2;
+   }
 }
 
 extern "C" void _start() {
-  const char *name = "Flying Chicken || Revision 1.0.2";
-  const bool found = 1;
-  
-  if (found == true) {
-     // pass
-  } else {
-     // pass
-  }
+   
+   const bool found = 1;
+   if (found == true) {
+      // pass
+   } else {
+      // pass
+   }
 
 }
