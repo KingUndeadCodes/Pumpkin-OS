4d36f1449b9ed264f917e53eb36aee9743eafee1
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-03-15 22:31:05 -0500

    Add files via upload

---
+++ print.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+static void system_print(uint8_t color) {
+   char* video_memory = (char*) 0xb8000;
+   char string[] = "Welcome to Pumpkin OS! (Flying Chicken || Revision 1.0.2)";
+   int length = sizeof(string) / sizeof(char);
+   for (int i = 0; i < length; i++) {
+      *video_memory++ = string[i];
+      *video_memory++ = color;
+   }
+   return;
+}
