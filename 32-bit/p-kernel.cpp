2e4c90375d40068cc6e0ce88d9bedb1cb9c85d62
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-03-18 11:56:02 -0500

    Update p-kernel.cpp

--- 32-bit/p-kernel.cpp
+++ 32-bit/p-kernel.cpp
@@ -1,12 +1,23 @@
+#include <stdint.h>
+
 /// Copyright (C) 2021 KingUndeadCodes (https://github.com/KingUndeadCodes)
 /// Protected under MIT License which lays down the terms of use.
 
-#include "print.c"
+void printk(uint8_t color) {
+   char* video_memory = (char*) 0xb8000;
+   char string[] = "Welcome to Pumpkin OS! (Flying Chicken || Revision 1.0.2)";
+   int length = sizeof(string) / sizeof(char);
+   for (int i = 0; i < length; i++) {
+      *video_memory++ = string[i];
+      *video_memory++ = color;
+   }
+   return;
+}
 
-extern "C" void _start() {
+extern "C" void _start(void) {
    const bool found = 1;
    if (found == true) {
-      // pass
+      printk(15);
    } else {
       // pass
    }
