ce830767055db701005ea95302686fbab3a88dd7
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-03-18 21:46:45 -0500

    Update p-kernel.cpp

--- 32-bit/p-kernel.cpp
+++ 32-bit/p-kernel.cpp
@@ -1,19 +1,11 @@
-/// Copyright (C) 2021 KingUndeadCodes (https://github.com/KingUndeadCodes)
+/// Copyright (C) 2020 KingUndeadCodes (https://github.com/KingUndeadCodes)
 /// Protected under MIT License which lays down the terms of use.
 
 #include <stdbool.h>
 #include <stdint.h>
-#include <stddef.h>
 
-// Going to add this at some point.
-static const size_t WIDTH = 80;
-static const size_t HEIGHT = 25;
 
-// From: https://github.com/Absurdponcho/YoutubeOS/blob/1392693673d02b38bb291fead334a706d423ec51/IO.cpp
-void outb(unsigned short port, unsigned char val){
-  asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
-}
-
+// Print function
 void printk(uint8_t color) {
    char* video_memory = (char*) 0xb8000;
    char string[] = "Welcome to Pumpkin OS! (Flying Chicken || Revision 1.0.2)";
@@ -24,10 +16,25 @@ void printk(uint8_t color) {
    }
 }
 
+
+// Input \ Output
+void outb(unsigned short port, unsigned char val){
+  asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+unsigned char inb(unsigned short port){
+  unsigned char returnVal;
+  asm volatile ("inb %1, %0"
+  : "=a"(returnVal)
+  : "Nd"(port));
+  return returnVal;
+}
+
+// Main function
 extern "C" void _start() {
-   const bool found = 1;
+   const bool found = true;
    if (found == true) {
-      printk(15);
+      // pass
    } else {
       // pass
    }
