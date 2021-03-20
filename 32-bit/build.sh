74ebafe40b4e89e4635c827d593f0376520dcf10
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-03-20 10:55:32 -0500

    Add files via upload

--- 32-bit/build.sh
+++ 32-bit/build.sh
@@ -1,15 +1,12 @@
-# DEFENITIONS
-comp=/usr/local/i386elfgcc/bin/./i386-elf-gcc
-link=/usr/local/i386elfgcc/bin/./i386-elf-ld
-qemu=qemu-system-x86_64
+function build {
+   /usr/local/i386elfgcc/bin/./i386-elf-gcc -ffreestanding -c p-kernel.cpp -o kernel.o
+   nasm Kernel-Entry.asm -f elf -o Kernel-Entry.o
+   /usr/local/i386elfgcc/bin/./i386-elf-ld -o kernel.bin -Ttext 0x1000 kernel.o Kernel-Entry.o --oformat binary
+   nasm bootLoader.asm -f bin -o Pump.bin
+   cat Pump.bin kernel.bin > image.bin
+   rm Kernel-Entry.o kernel.bin Pump.bin kernel.o
+   qemu-system-x86_64 -drive format=raw,file=image.bin,if=floppy
+   rm image.bin
+}
 
-# ASSEMBLERS, COMPILERS AND LINKERS
-${comp} -ffreestanding -c p-kernel.cpp -o kernel.o
-nasm Kernel-Entry.asm -f elf -o Kernel-Entry.o
-${link} -o kernel.bin -Ttext 0x1000 kernel.o Kernel-Entry.o --oformat binary
-nasm bootLoader.asm -f bin -o Pump.bin
-cat Pump.bin kernel.bin > image.bin
-
-# VIRTUAL MACHINE AND DELETE TEMP FILES
-rm Kernel-Entry.o kernel.bin Pump.bin kernel.o
-${qemu} -drive format=raw,file=image.bin,if=floppy
+build now
