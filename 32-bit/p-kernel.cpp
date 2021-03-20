d120781842feff6a6c22674b70579bfd4188f3f7
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-03-20 10:56:53 -0500

    Add files via upload

--- 32-bit/p-kernel.cpp
+++ 32-bit/p-kernel.cpp
@@ -3,7 +3,7 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-#include "io.h"
+
 
 // Print function
 void printk(uint8_t color) {
@@ -16,11 +16,33 @@ void printk(uint8_t color) {
    }
 }
 
+
+// Input \ Output
+void outb(unsigned short port, unsigned char val){
+  asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+unsigned char inb(unsigned short port){
+  unsigned char returnVal;
+  asm volatile ("inb %1, %0"
+  : "=a"(returnVal)
+  : "Nd"(port));
+  return returnVal;
+}
+
+void enable_cursor(uint8_t cursor_start, uint8_t cursor_end)
+{
+   outb(0x3D4, 0x0A);
+   outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start);
+   outb(0x3D4, 0x0B);
+   outb(0x3D5, (inb(0x3D5) & 0xE0) | cursor_end);
+}
+
 // Main function
 extern "C" void _start() {
    const bool found = true;
    if (found == true) {
-      // pass
+      enable_cursor(255, 255);
    } else {
       // pass
    }
