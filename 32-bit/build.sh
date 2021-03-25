6e499023cf9e646df4a873fc84a71bff11726f8e
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-03-25 10:39:11 -0500

    Update build.sh

--- 32-bit/build.sh
+++ 32-bit/build.sh
@@ -2,7 +2,7 @@ function build {
    /usr/local/i386elfgcc/bin/./i386-elf-gcc -ffreestanding -c p-kernel.cpp -o kernel.o
    nasm Kernel-Entry.asm -f elf -o Kernel-Entry.o
    nasm empty_end.asm -f bin -o empty_end.bin
-   /usr/local/i386elfgcc/bin/./i386-elf-ld -o kernel.bin -Ttext 0x1000 kernel.o Kernel-Entry.o --oformat binary
+   /usr/local/i386elfgcc/bin/./i386-elf-ld -o kernel.bin -Ttext 0x1000 Kernel-Entry.o kernel.o --oformat binary
    nasm bootLoader.asm -f bin -o Pump.bin
    cat Pump.bin kernel.bin > short.bin
    cat short.bin empty_end.bin > image.bin
