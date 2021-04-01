a30958782437438ed506bccad9a35cbdf1bbc7bd
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-04-01 13:02:31 -0500

    Update p-kernel.cpp

--- 32-bit/p-kernel.cpp
+++ 32-bit/p-kernel.cpp
@@ -15,18 +15,6 @@ void println() {
    };
 };
 
-void outb(unsigned short port, unsigned char val) {
-   asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
-};
-
-unsigned char inb(unsigned short port) {
-   unsigned char returnVal;
-   asm volatile ("inb %1, %0"
-   : "=a"(returnVal)
-   : "Nd"(port));
-   return returnVal;
-};
-
 extern "C" void _start() {
    println();
 };
