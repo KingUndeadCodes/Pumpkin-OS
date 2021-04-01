a7ab2611c61c91ef7576d76a153ececbcfd80c7e
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-04-01 15:11:14 -0500

    Update boot.s

--- boot.s
+++ boot.s
@@ -1,10 +1,19 @@
 /*
- * Experimental booting process for ARM (Still testing):
- *    - Copyright King Undead 2021.
+ * CODE (7-21) || https://stackoverflow.com/questions/6139952/what-is-the-booting-process-for-arm
 */
 
-.section .text
-.global _start
+.globl _start
+ _start: b       reset
+    ldr     pc, _undefined_instruction
+    ldr     pc, _software_interrupt
+    ldr     pc, _prefetch_abort
+    ldr     pc, _data_abort
+    ldr     pc, _not_used
+    ldr     pc, _irq
+    ldr     pc, _fiq
 
-  _start:
-    // pass
+reset:
+    mrs     r0,cpsr                 /* set the cpu to SVC32 mode        */
+    bic     r0,r0,#0x1f             /* (superviser mode, M=10011)       */
+    orr     r0,r0,#0x13
+    msr     cpsr,r0
