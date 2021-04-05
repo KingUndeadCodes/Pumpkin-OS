ac437e7a18b925a4b7d945a1173a13b21f6e6c66
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-04-05 12:19:06 -0500

    Update incbins.asm

--- 32-bit/incbins.asm
+++ 32-bit/incbins.asm
@@ -1,8 +1,8 @@
 global _UwU
 global _OwO
 
-_UwU: incbin "../etc/UwU.txt"
-db 0
+; _UwU: incbin "../etc/UwU.txt"
+; db 0
 
-_OwO: incbin "../etc/OwO.txt"
-db 0
+; _OwO: incbin "../etc/OwO.txt"
+; db 0
