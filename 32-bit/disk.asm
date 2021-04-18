240c7335a82252bf96f1586214f051e3c47103d9
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-04-18 15:08:11 -0500

    Update disk.asm

--- 32-bit/disk.asm
+++ 32-bit/disk.asm
@@ -1,13 +1,13 @@
 disk_load:
   mov ah, 0x02 
-  mov al, dh      ; Read DH sectors
-  mov ch, 0x00    ; Select cylinder 0
-  mov dh, 0x00    ; Select head 0
-  mov cl, 0x02    ; Start reading from second sector ( i.e. after the boot sect$
+  mov al, dh          ; Read DH sectors
+  mov ch, 0x00        ; Select cylinder 0
+  mov dh, 0x00        ; Select head 0
+  mov cl, 0x02        ; Start reading from second sector ( i.e. after the boot sect$
   mov dl, [BOOT_DISK]
-  int 0x13        ; BIOS interrupt
-  jc disk_error   ; Jump if error ( i.e. carry flag set )
-  cmp ah, 0       ; if AL ( sectors read ) != DH ( sectors expected )
+  int 0x13            ; BIOS interrupt
+  jc disk_error       ; Jump if error ( i.e. carry flag set )
+  cmp ah, 0           ; if AL ( sectors read ) != DH ( sectors expected )
   jne disk_error
   ret
 
