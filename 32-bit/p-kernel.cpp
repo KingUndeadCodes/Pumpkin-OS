9bb5c4a5424b5ad2fe9149e561e5fbc9894e6fdc
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-04-22 11:47:02 -0500

    Update p-kernel.cpp

--- 32-bit/p-kernel.cpp
+++ 32-bit/p-kernel.cpp
@@ -3,6 +3,15 @@
  * Protected under MIT License which lays down the terms of use.
 */
 
+#include <stdint.h>
+
+void WriteCharacter(unsigned char c, unsigned char forecolour, unsigned char backcolour, int x, int y) {
+     uint16_t attrib = (backcolour << 4) | (forecolour & 0x0F);
+     volatile uint16_t * where;
+     where = (volatile uint16_t *)0xB8000 + (y * 80 + x) ;
+     *where = c | (attrib << 8);
+}
+
 void println() {
    volatile char *video_memory = (volatile char*)0xB8000;
    char string[] = "Pumpkin OS - 4-14-2021 - MIT";
