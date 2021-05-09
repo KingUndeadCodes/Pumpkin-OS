0263df6ccbb78b14244bdce4b34948e5bdac5b78
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-05-09 16:58:50 -0500

    Update p-kernel.cpp

--- 32-bit/p-kernel.cpp
+++ 32-bit/p-kernel.cpp
@@ -7,7 +7,7 @@
 
 inline void println(uint8_t color = 10) {
     volatile char *video_memory = (volatile char*)0xb8000;
-    char string[] = "[1] Found.;
+    char string[] = "[1] Found...";
     long length = sizeof(string) / sizeof(char);
     for (int i = 0; i < length; i++) {
        video_memory[2 * i] = string[i];
