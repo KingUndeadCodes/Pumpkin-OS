06bfebaab127cbe2a832ca050fd9b237d85076d9
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-05-16 19:50:51 -0500

    Update p-kernel.cpp

--- 32-bit/p-kernel.cpp
+++ 32-bit/p-kernel.cpp
@@ -3,18 +3,8 @@
  * Protected under MIT License which lays down the terms of use.
 */
 
-#include <stdint.h>
-
-inline void println(uint8_t color = 10) {
-    volatile char *video_memory = (volatile char*)0xb8000;
-    char string[] = "[1] Found...";
-    long length = sizeof(string) / sizeof(char);
-    for (int i = 0; i < length; i++) {
-       video_memory[2 * i] = string[i];
-       video_memory[2 * i + 1] = color;
-    };
-};
+#include "mods/text.cpp"
 
 extern "C" void _start() {
-    println(5);
+    printf("Alive!", 10);
 }
