5a7f8d1d8ac24547b047fa6d07e4a1ca57bc2bf7
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-05-16 19:49:56 -0500

    Create text.cpp

---
+++ 32-bit/mods/text.cpp
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+static inline unsigned int strlen(char* str) {
+    unsigned int len = 0;
+    while (str[len] && str[len] != '\0') {len++;}
+    return len;
+}
+
+inline void printf(char* string, uint8_t color = 15) {
+    volatile char *video_memory = (volatile char*)0xb8000;
+    long length = strlen(string);
+    for (int i = 0; i < length; i++) {
+       video_memory[2 * i] = string[i];
+       video_memory[2 * i + 1] = color;
+    }
+}
