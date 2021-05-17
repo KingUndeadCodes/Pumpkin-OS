ea6f807773fc20bf70f60071ab38136efc88e1d1
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-05-16 20:49:19 -0500

    Update disk.asm

--- 32-bit/disk.asm
+++ 32-bit/disk.asm
@@ -1,28 +1,26 @@
 disk_load:
-  mov ah, 0x02 
-  mov al, dh          ; Read DH sectors
-  mov ch, 0x00        ; Select cylinder 0
-  mov dh, 0x00        ; Select head 0
-  mov cl, 0x02        ; Start reading from second sector ( i.e. after the boot sect$
-  mov dl, [BOOT_DISK]
-  int 0x13            ; BIOS interrupt
-  jc disk_error       ; Jump if error ( i.e. carry flag set )
-  cmp ah, 0           ; if AL ( sectors read ) != DH ( sectors expected )
-  jne disk_error
-  ret
+    mov ah, 0x02
+    mov al, dh      ; Read DH sectors
+    mov ch, 0x00    ; Select cylinder 0
+    mov dh, 0x00    ; Select head 0
+    mov cl, 0x02    ; Start reading from second sector ( i.e. after the boot sect$
+    mov dl, [BOOT_DISK]
+    int 0x13        ; BIOS interrupt
+    jc disk_error   ; Jump if error ( i.e. carry flag set )
+    cmp ah, 0       ; if AL ( sectors read ) != DH ( sectors expected )
+    jne disk_error
+    ret
 
 BOOT_DISK: db 0
 
 disk_error:
-  mov ah, 0x0e
-  mov al, 'E'
-  int 0x10
-  mov al, 'r'
-  int 0x10
-  mov al, 'r'
-  int 0x10
-  mov al, 'o'
-  int 0x10
-  mov al, 'r'
-  int 0x10
-  jmp $
+    mov ah, 0x0e
+    mov al, 0x45
+    int 0x10
+    mov al, 0x72
+    int 0x10
+    mov al, 0x72
+    int 0x10
+    mov al, 0x6f
+    int 0x10
+    hlt
