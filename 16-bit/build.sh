624874f1f4da5cb76bac54343fab6be9bc027b17
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-05-16 20:55:27 -0500

    Update build.sh

--- 16-bit/build.sh
+++ 16-bit/build.sh
@@ -1,7 +1,3 @@
-function build {
-   nasm bootLoader.asm -f bin -o image.bin
-   qemu-system-x86_64 -drive format=raw,file=image.bin,if=floppy
-   rm image.bin
-}
-
-build now
+nasm bootLoader.asm -f bin -o image.bin
+qemu-system-x86_64 -drive format=raw,file=image.bin,if=floppy
+rm image.bin
