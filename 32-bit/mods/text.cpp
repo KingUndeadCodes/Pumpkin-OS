ffc92b2efce51097b11dbba0b5f71b14cf9f8dba
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-05-20 14:20:18 -0500

    Update text.cpp

--- 32-bit/mods/text.cpp
+++ 32-bit/mods/text.cpp
@@ -1,22 +1,22 @@
 #include <stdint.h>
 
 enum COLORS {
-    COLRO_BLACK = 0;
-    COLOR_BLUE = 1;
-    COLOR_GREEN = 2;
-    COLOR_CYAN = 3;
-    COLOR_RED = 4;
-    COLOR_PURPLE = 5;
-    COLOR_BROWN = 6;
-    COLOR_GRAY = 7;
-    COLOR_DARK_GRAY = 8;
-    COLOR_LIGHT_BLUE = 9;
-    COLOR_LIGHT_GREEN = 10;
-    COLOR_LIGHT_CYAN = 11;
-    COLOR_LIGHT_RED = 12;
-    COLOR_LIGHT_PURPLE = 13;
-    COLOR_YELLOW = 14;
-    COLOR_WHITE = 15;
+    COLOR_BLACK = 0,
+    COLOR_BLUE = 1,
+    COLOR_GREEN = 2,
+    COLOR_CYAN = 3,
+    COLOR_RED = 4,
+    COLOR_PURPLE = 5,
+    COLOR_BROWN = 6,
+    COLOR_GRAY = 7,
+    COLOR_DARK_GRAY = 8,
+    COLOR_LIGHT_BLUE = 9,
+    COLOR_LIGHT_GREEN = 10,
+    COLOR_LIGHT_CYAN = 11,
+    COLOR_LIGHT_RED = 12,
+    COLOR_LIGHT_PURPLE = 13,
+    COLOR_YELLOW = 14,
+    COLOR_WHITE = 15,
 };
 
 static inline unsigned int strlen(char* str) {
