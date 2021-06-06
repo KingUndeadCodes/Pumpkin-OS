c7a720fe70a18011c371f3f3320e109a9b79dcbe
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-06-06 11:14:04 -0500

    Update p-kernel.cpp

--- 32-bit/p-kernel.cpp
+++ 32-bit/p-kernel.cpp
@@ -8,16 +8,6 @@
 #include "mods/dev/PIC.cpp"
 
 extern "C" void _start() {
-    printf("Success", COLOR_LIGHT_GREEN);
-    char c = 0;
     init_pics(0x20, 0x28);
-    do {
-        if(inb(0x60) != c) {
-            c = inb(0x60);
-            if(c > 0) {
-                printf(c, COLOR_LIGHT_GREEN);
-            }
-        }
-    }
-    while(c != 1);
+    printf("Success", COLOR_LIGHT_GREEN);
 }
