29ee5133a0cdba86778a14fc810bb842edf45545
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-07-12 10:46:20 -0500

    Create text.h

---
+++ 32-bit/mods/std/text.h
@@ -0,0 +1,24 @@
+#include "stdint.h"
+#pragma once
+
+enum {
+    COLOR_BLACK = 0x0,
+    COLOR_BLUE = 0x1,
+    COLOR_GREEN = 0x2,
+    COLOR_CYAN = 0x3,
+    COLOR_RED = 0x4,
+    COLOR_PURPLE = 0x5,
+    COLOR_BROWN = 0x6,
+    COLOR_GRAY = 0x7,
+    COLOR_DARK_GRAY = 0x8,
+    COLOR_LIGHT_BLUE = 0x9,
+    COLOR_LIGHT_GREEN = 0xA,
+    COLOR_LIGHT_CYAN = 0xB,
+    COLOR_LIGHT_RED = 0xC,
+    COLOR_LIGHT_PURPLE = 0xD,
+    COLOR_YELLOW = 0xE,
+    COLOR_WHITE = 0xF,
+};
+
+static u32 strlen(char* string);
+void printf(char* string, u8 color);
