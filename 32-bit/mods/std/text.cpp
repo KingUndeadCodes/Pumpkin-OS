8af72d395a6eae186b4b5f7c528632557b63c70a
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-07-12 10:46:49 -0500

    Update text.cpp

--- 32-bit/mods/std/text.cpp
+++ 32-bit/mods/std/text.cpp
@@ -1,23 +1,4 @@
-#include "stdint.h"
-
-enum {
-    COLOR_BLACK = 0x0,
-    COLOR_BLUE = 0x1,
-    COLOR_GREEN = 0x2,
-    COLOR_CYAN = 0x3,
-    COLOR_RED = 0x4,
-    COLOR_PURPLE = 0x5,
-    COLOR_BROWN = 0x6,
-    COLOR_GRAY = 0x7,
-    COLOR_DARK_GRAY = 0x8,
-    COLOR_LIGHT_BLUE = 0x9,
-    COLOR_LIGHT_GREEN = 0xA,
-    COLOR_LIGHT_CYAN = 0xB,
-    COLOR_LIGHT_RED = 0xC,
-    COLOR_LIGHT_PURPLE = 0xD,
-    COLOR_YELLOW = 0xE,
-    COLOR_WHITE = 0xF,
-};
+#include "text.h"
 
 static u32 strlen(char* string) {
     u32 len = 0;
