d516c30ad12abb7b71ab47612022d2d76612e672
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-08-29 21:26:52 -0500

    Update build.sh

--- 32-bit/build.sh
+++ 32-bit/build.sh
@@ -1,27 +1,3 @@
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# AS      =nasm													    #
-# CC      =i386-elf-g++												    #
-# LD      =i386-elf-ld												    #
-# CFLAGS  =-fpermissive -ffreestanding -m32 -g -c -w -I mods/std/include -o2					    #
-#														    #
-# all: barebones clean												    #
-#														    #
-# barebones:													    #
-#         $(AS) bootLoader.asm -f bin -o Pump.bin								    #
-#         $(AS) empty_end.asm -f bin -o empty_end.bin								    #
-#         $(AS) Kernel-Entry.asm -f elf -o Kernel-Entry.o							    #
-#         $(CC) $(CFLAGS) p-kernel.cpp -o kernel.o								    #
-#         $(CC) $(CFLAGS) mods/std/include/string.cpp -o string.o						    #
-#         $(CC) $(CFLAGS) mods/std/text.cpp -o text.o								    #
-#         $(CC) $(CFLAGS) mods/dev/PIC.cpp -o PIC.o								    #
-#         $(LD) -o kernel.bin -Ttext 0x1000 Kernel-Entry.o kernel.o text.o PIC.o string.o --oformat binary	    #
-#         cat Pump.bin kernel.bin > short.bin									    #
-#         cat short.bin empty_end.bin > image.bin								    #
-#														    #
-# clean:													    #
-#         rm image.bin Kernel-Entry.o kernel.bin Pump.bin kernel.o text.o PIC.o empty_end.bin short.bin string.o    #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
 function build {
     clear
     export PATH=$PATH:/usr/local/i386elfgcc/bin
