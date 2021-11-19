d7cb3e808d92f3ca11dbad71f245c87ea3c1ea9d
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2021-11-18 20:01:32 -0600

    Update p-kernel.cpp

--- 32-bit/p-kernel.cpp
+++ 32-bit/p-kernel.cpp
@@ -3,13 +3,14 @@
  * Protected under MIT License which lays down the terms of use.
 */
 
-#include "mods/std/text.h"
 #include "mods/dev/PIC.h"
+#include <text.h>
 
 extern "C" void _start() {
     Cursor::enableCursor(0, 10);
     printf("Booting PumpkinOS (ver: 0)\n", COLOR_CYAN | COLOR_BLACK << 4);
-    // x86 Hardware Initialization
-    init_pics(0x20, 0x28);
-    printf("\nInitialized PIC!");
+    #if defined(__x86_64__) || defined(_M_X64) || defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+        init_pics(0x20, 0x28);
+        printf("\nInitialized PIC!");
+    #endif
 }
