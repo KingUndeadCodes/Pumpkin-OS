3bfdeefa010801217f2cb0a186fec65d1734cb17
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2022-01-01 23:07:18 -0600

    Fixed the “bug”
    It’s really just a function I used to experiment with.

--- 32-bit/mods/std/text.cpp
+++ 32-bit/mods/std/text.cpp
@@ -49,21 +49,3 @@ void printf(const char* string, uint8_t color = 15) {
        Cursor::moveCursor(col - 1, row);
     }
 }
-
-void scanf(struct regs *r) {
-    	const char kbdus[128] = {
-        	0,  27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b', '\t',
-        	'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', 0,
-        	'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0,
-        	'\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0,
-        	'*', 0, ' ', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        	'-', 0, 0, 0, '+', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    	};
-	unsigned char scancode;
-	scancode = inb(0x60);
-	if (scancode & 0x80) {
-		// pass
-	} else {
-		printf(kbdus[scancode]);
-	}
-}
